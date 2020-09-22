b794d8c7c66df507e7b4cd984284ca0b38ace1e4
fzm <fanzhengmaot@163.com>
2020-09-22 23:50:55 +0800

    合并链表

---
+++ ag_21/index.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+const mergeTwoLists = function (l1, l2) {
+  const root = new ListNode(-1);
+  let pre = root;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      pre.next = l1;
+      l1 = l1.next;
+    } else {
+      pre.next = l2;
+      l2 = l2.next;
+    }
+    pre = pre.next;
+  }
+  pre.next = l1 || l2;
+  return root.next;
+};
+
+const l1 = new ListNode(1);
+l1.next = new ListNode(2);
+l1.next.next = new ListNode(4);
+
+const l2 = new ListNode(1);
+l2.next = new ListNode(3);
+l2.next.next = new ListNode(4);
+
+
+const res = mergeTwoLists(l1, l2);
+console.log("res", res)
